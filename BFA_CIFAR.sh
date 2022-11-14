e628e995439be6f49cdd013352c99c20c50e4b93
Zahra Heydari <98219883+ZahraHeydari95@users.noreply.github.com>
2022-11-14 19:41:38 +0330

    Update BFA_CIFAR.sh

--- BFA_CIFAR.sh
+++ BFA_CIFAR.sh
@@ -7,17 +7,17 @@ echo "Current host is: $HOST"
 # Automatic check the host and configure
 case $HOST in
 "alpha")
-    PYTHON="/home/elliot/anaconda3/envs/pytorch041/bin/python" # python environment path
-    TENSORBOARD='/home/elliot/anaconda3/envs/pytorch041/bin/tensorboard' # tensorboard environment path
-    data_path='/home/elliot/data/pytorch/cifar10'
+    TENSORBOARD='/usr/local/bin/tensorboard' # tensorboard environment path
+    data_path='/content/data/cifar-10-batches-py'
     ;;
 esac
 
 DATE=`date +%Y-%m-%d`
 
-if [ ! -d "$DIRECTORY" ]; then
-    mkdir ./save/${DATE}/
-fi
+mkdir save
+cd save 
+mkdir ${DATE}
+cd ..
 
 ############### Configurations ########################
 enable_tb_display=false # enable tensorboard display
@@ -35,13 +35,13 @@ save_path=./save/${DATE}/${dataset}_${model}_${label_info}
 tb_path=${save_path}/tb_log  #tensorboard log path
 
 # set the pretrained model path
-pretrained_model=/home/elliot/Documents/CVPR_2020/BFA_defense_camera_ready/BFA/save/2020-05-08/cifar10_resnet20_quan_160_SGD_binarized/model_best.pth.tar
+pretrained_model=/content/BFA/save/{DATE}/cifar10_resnet20_quan_160_SGD_binarized/model_best.pth.tar
 
 ############### Neural network ############################
 COUNTER=0
 {
 while [ $COUNTER -lt 1 ]; do
-    $PYTHON main.py --dataset ${dataset} \
+    python main.py --dataset ${dataset} \
         --data_path ${data_path}   \
         --arch ${model} --save_path ${save_path}  \
         --test_batch_size ${test_batch_size} --workers 8 --ngpu 1 --gpu_id 1 \
